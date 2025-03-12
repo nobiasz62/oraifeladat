d3fe926f6ca8e259a6e339d8e70ddb72eda3c27f
nobiasz62 <acsnor055@hengersor.hu>
2025-03-12 11:29:59 +0100

    orai

---
+++ felveteli.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const app = express();
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    user:"root",
+    host:"localhost",
+    port:3306,
+    password:"",
+    database:"felveteli"
+});
+
+app.get("/", (req, res) => {
+    res.send("A szerver működik.")
+})
+
+app.get("/jelentkezesek", (req, res) => {
+    db.query("SELECT * FROM jelentkezesek", (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log("A felveteli szerver a 3000-es porton fut.")
+})
