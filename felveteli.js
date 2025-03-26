d2afa93d01cba5196b6b1f51a1514b1174a02026
nobiasz62 <acsnor055@hengersor.hu>
2025-03-26 10:51:12 +0100

    gyak

--- felveteli.js
+++ felveteli.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 const bodyparser = require("body-parser");
-const app = express();
+const app = express();  
 
 app.use(cors());
 
